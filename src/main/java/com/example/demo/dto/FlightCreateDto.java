fc9c5ce8eec7435bd60ccbad6663513c9d1746c1
Denis Savitskiy <savickijdenis69@gmail.com>
2026-04-03 14:06:03 +0300

    LAB4 add discriptions

--- src/main/java/com/example/demo/dto/FlightCreateDto.java
+++ src/main/java/com/example/demo/dto/FlightCreateDto.java
@@ -17,18 +17,25 @@ import java.util.List;
 @AllArgsConstructor
 public class FlightCreateDto {
     @NotBlank(message = "FlightNumber is obligatory")
+    @Schema(description = "Номер рейса", example = "su-130", required = true)
     private String flightNumber;
     @NotNull(message = "DepartureDate is obligatory")
+    @Schema(description = "Дата отправления рейса ", example = "2026-03-26", required = true)
     private LocalDate departureDate;
     @NotNull(message = "ArrivalDate is obligatory")
+    @Schema(description = "Дата прилета", example = "2026-03-26", required = true)
     private LocalDate arrivalDate;
     @NotNull(message = "DepartureAirportCode is obligatory")
+    @Schema(description = "Id аэропорта вылета", example = "1", required = true)
     private Long departureAirportCode;
     @NotNull(message = "ArrivalAirportCode is obligatory")
+    @Schema(description = "Id аэропорта прилета ", example = "1", required = true)
     private Long arrivalAirportCode;
     @NotNull(message = "AirplaneId is obligatory")
+    @Schema(description = "Id самолета ", example = "1", required = true)
     private Long airplaneId;
 
+    @Schema(description = "Id услуг", example = "1,2", required = true)
     private List<Long> amenities;
 
     @NotNull(message = "FlightStatus is obligatory")
--- src/main/java/com/example/demo/dto/TicketCreateDto.java
+++ src/main/java/com/example/demo/dto/TicketCreateDto.java
@@ -1,5 +1,6 @@
 package com.example.demo.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
@@ -12,13 +13,18 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class TicketCreateDto {
     @NotBlank(message = "PassportNumber is obligatory")
+    @Schema(description = "Номер паспорта", example = "BM2746542", required = true)
     private String passportNumber;
     @NotBlank(message = "PassengerName is obligatory")
+    @Schema(description = "Имя пассажира", example = "Иван Иванов", required = true)
     private String passengerName;
+    @Schema(description = "Номер места", example = "A39", required = true)
     @NotBlank(message = "Seat is obligatory")
     private String seat;
+    @Schema(description = "Цена", example = "109.3", required = true)
     @Positive(message = "Price must be positive")
     private double price;
+    @Schema(description = "Номер рейса", example = "1", required = true)
     @NotNull(message = "FlightId is obligatory")
     private Long flightId;
 }
